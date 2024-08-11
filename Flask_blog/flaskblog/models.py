c4f0c1d124c9926f2bec8828d8b0871471a5ff96
the-projects <161836669+Jaymestalabistudent@users.noreply.github.com>
2024-08-11 14:36:18 +0100

    OrchidOs : User Authentication ;  Login Logout Routes
    Refactored HTML templates for better structure and security
    - Cleaned up and organized layoutuser.html, layout.html, login.html, and navbaruser.html.
    - Ensured proper routing and template inheritance to enhance maintainability.
    - Improved security by following best practices in template design and caching
    - Reduced the need for extensive unit tests by adhering to best practices and ensuring a consistent structure.
     -- Added flask-login
     --Added @login_required to all routes
    These changes enable a more secure website and streamline the development process, allowing for easier future enhancements and maintenance.

--- Flask_blog/flaskblog/models.py
+++ Flask_blog/flaskblog/models.py
@@ -1,26 +1,31 @@
-from datetime import datetime, timezone
-from flaskblog import db
+from datetime import datetime
+from flaskblog import db, login_manager
+from flask_login import UserMixin
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg') # default image for user profile will uploaded later
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)  # lazy loading to get all the post of the user in one query relationship between the post and the user
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}'), {self.image_file}" # this is how the object is printed out
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc)) # default  time zone
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # foreign key to the user table this is the link between the files so that the post and the user are connected
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')" # this is how the object is printed out
+        return f"Post('{self.title}', '{self.date_posted}')"
